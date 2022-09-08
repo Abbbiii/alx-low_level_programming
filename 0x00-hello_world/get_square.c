68330a48c172b318c5b0b3bc41679b285d45b258
Abbbiii <brtabssn@gmail.com>
2022-09-08 14:29:36 -0700

    Betty1

---
+++ 0x00-hello_world/get_square.c
@@ -0,0 +1,15 @@
+  #include <stdio.h>
+  
+  /**
+  * getSquare - get square of number passed
+  * @num: number to be squared
+  * Return: squared num or -1
+  */
+  int getSquare(int num)
+  {
+	 if (sizeof(num) === sizeof(int))
+	{
+		return(num ** 2);
+	}
+	print("Please enter a valid number!\n");
+	return (-1);
