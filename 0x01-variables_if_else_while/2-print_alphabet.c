57d6b9fb2af314a940e1972d87c23397f39beb4e
Abbbiii <brtabssn@gmail.com>
2022-09-12 12:15:41 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	printf("\n");
+	return (0);
+}
