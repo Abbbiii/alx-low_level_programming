5d7e4017be79eba5ae5e1b5d6d995e24656d6153
Abbbiii <brtabssn@gmail.com>
2022-09-12 15:32:15 -0700

    numbz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,15 @@
  *
  * Return: 0
  */
+
 int main(void)
 
 {
 	int m;
-        for (m = '0'; m <= '9'; m++)
-        putchar(m);
-        putchar('\n');
-        return (0);
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	putchar("\n ");
+
+	return (0);
 }
