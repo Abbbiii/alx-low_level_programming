18c69560edf5b96b34d19ef47e8c0e4260ea24c5
Abbbiii <brtabssn@gmail.com>
2022-09-12 15:20:22 -0700

    digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	int m;
+        for (m = '0'; m <= '9'; m++)
+        putchar(m);
+        putchar('\n');
+        return (0);
+}
