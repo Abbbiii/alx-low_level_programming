de1a55bed7d379117e597388389a515c7ee769da
Abbbiii <brtabssn@gmail.com>
2022-09-12 19:37:52 -0700

    teba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	for (low = 'z'; low >= 'a'; low ==)
+		putchar(low);
+	putchar("\n");
+	return (0);
+}
