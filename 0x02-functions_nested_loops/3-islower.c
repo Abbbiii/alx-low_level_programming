644a1b3a853ee41102f5b8e6d5d1ad9f53eaf8b9
Abbbiii <brtabssn@gmail.com>
2022-09-13 19:28:58 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+/**
+ * is lower - prints or checks the lower alphabet
+ *
+ *
+ * * Return: returnes 1 and 0 depening on condition
+ *
+ * */
+
+int _islower(int c)
+
+{
+
+	return (c >= 'a' && c <= 'z');
+
+}
