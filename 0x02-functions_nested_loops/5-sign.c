e9bc2037602799e3686f2acd780ca60e28e31c72
Abbbiii <brtabssn@gmail.com>
2022-09-17 05:37:27 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,24 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
-*more_numbers - prints numbers to 14
-*
-*Return: returns nothing
-*/
-
-void more_numbers(void)
+ * print_sign - function to check for a sign of a number
+ * @n:  is the int that will use for the argument of the function
+ * Return: 0
+ */
+int print_sign(int n)
 {
-	int n1, n2;
-
-	for (n1 = 0; n1 < 10; n1++)
+	if (n > 0)
 	{
-		for (n2 = 0; n2 <= 14; n2++)
-		{
-			if (n2 > 9)
-			{
-				putchar((n2 / 10) + '0');
-			}
-			putchar((n2 % 10) + '0');
-		}
-		putchar(10);
+	_putchar('+');
+	return (1);
+}
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
 	}
 }
