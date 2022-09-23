b696371bccbf3f399219cf2cae247f5de17bc320
Abbbiii <brtabssn@gmail.com>
2022-09-23 06:32:58 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * strcat - use @src and @dest
+ * _strcat - use @src and @dest
  * @dest: A pointer to the string to be concentrated upon.
  * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
 
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 	while (dest[index++])
