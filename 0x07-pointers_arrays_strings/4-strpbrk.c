a36495bbd18dce3a526cf0cd0d22c01b1c8d5a46
Abbbiii <brtabssn@gmail.com>
2022-09-26 23:02:48 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,10 +8,11 @@
  * Return: pointers to byte in s that matches or null if no match
  */
 
+char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i]; i++);  
+	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
