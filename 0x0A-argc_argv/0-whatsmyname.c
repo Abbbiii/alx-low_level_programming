274ddd55169a6a75ad07c6d8f066426d6623d5e5
Abbbiii <brtabssn@gmail.com>
2022-10-01 01:42:54 -0700

    Argument count

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,14 @@
 
 /**
  * main - Print the name of the program
+ * @argv: the argument
  * @argc: Count arguments
- * @argv: arguments
  *
  * Return: Always 0 (success)
  */
-
 int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s\n". argv[0]);
-
-return (0);
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
 }
