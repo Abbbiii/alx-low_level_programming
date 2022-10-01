64ff23b1100f63baa18bc6c6c749cedbe569cbdb
Abbbiii <brtabssn@gmail.com>
2022-10-01 03:24:18 -0700

    Arguments recieved

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all the arguments a program recieves
+ *
+ * @argc: number of arguments.
+ * @argv: the argument
+ *
+ * Return: Always 0 if success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[]);
+	}
+	return (0);
+}
